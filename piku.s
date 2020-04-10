45963d8a8232efee57063557a6945da4d9eba250
Michaelgal13 <michaelgal13@gmail.com>
2020-04-10 09:13:23 +0100

    Oh man I think I did it!

--- piku.s
+++ piku.s
@@ -10,6 +10,10 @@ main:
     @ Load the argument and perform the call. This is like 'printf("...")' in C.
     ldr     r0, =message
     bl      printf
+    ldr     r0, =message2
+    bl      printf
+    ldr     r0, =message3
+    bl      printf
 
     @ Exit from 'main'. This is like 'return 0' in C.
     mov     r0, #0      @ Return 0.
@@ -20,5 +24,18 @@ main:
     @ --------------------------------
     @ Data for the printf call. The GNU assembler's ".asciz" directive
     @ automatically adds a NULL character termination.
+
+
 message:
-    .asciz  "Hello, world.\n"
+    @.asciz  "Cal Poly's Servers.\n"
+    @.asciz "Cannot handle al students. \n"
+    @.asciz "This VPN sucks. \n"
+.asciz "Raspberry Pi 4. \n"
+message2:
+.asciz "How the heck do I use it? \n"
+message3:
+.asciz "Hopefully I'll learn.\n"
+
+    @.asciz "Segmentation Fault"
+    @.asciz "Guess that I did something wrong. \n"
+    @.asciz "Oh well, try again. \n"
